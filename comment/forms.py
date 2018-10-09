b7b2d1a83fdef16f280427e9167b045871c7a5ea
ahsanhabib98 <ahredoan@gmail.com>
2018-10-09 23:39:57 +0600

    updated commentsection

--- comment/forms.py
+++ comment/forms.py
@@ -6,4 +6,12 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content', 'name', 'email', ]
+        fields = '__all__'
+        widgets = {
+            'content_type': forms.HiddenInput(attrs={'class':'form-control'}),
+            'object_id': forms.HiddenInput(attrs={'class':'form-control'}),
+            'parent': forms.HiddenInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.TextInput(attrs={'class':'form-control'}),
+        }
--- comment/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import comment_thread
-
-
-urlpatterns = [
-    path('thread/<int:id>', comment_thread, name='thread'),
-]
